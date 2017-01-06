5280ed589d7b996f599d93b3b99b5f2bfde35fe3
Ana-Elisa Gentle <anaelisagentle@Ana-Elisas-MBP.fios-router.home>
2017-01-06 17:13:18 -0500

    answered part II

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,21 @@
-PLACE YOUR CODE HERE
+#Part II - Write CSV File
+
+#Q5 Write email addresses from Part I to csv file called emails.csv
+
+import csv
+
+file = csv.DictReader(open('faculty.csv'))
+
+#create list of emails
+l_email = []
+for row in file:
+	l_email.append(row[' email'])
+
+
+#create csv file with list of emails
+f = open("emails.csv", "wb")
+writer = csv.writer(f)
+for email in l_email:
+	writer.writerow([email,])
+f.close()
+
